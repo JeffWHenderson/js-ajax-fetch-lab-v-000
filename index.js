1c46a1016d576c838b82955850a3944e46206e00
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-01-04 18:14:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,10 +35,24 @@ function getIssues(data) {
     })
 }
 
+function displayIssue(issue) {
+  $('#issues').append(issue.template())
+}
+
 // function showIssues(json) {
 // }
 
 function createIssue() {
+  const issueTitle = document.getElementById('title').value
+  const issueBody = document.getElementById('body').value
+  const postData = { title: issueTitle, body: issueBody }
+  fetch(`${baseApi}repos/${fork}/issues`, {
+    method: 'post',
+    headers: {
+      'Authorization': `token ${getToken()}`
+    },
+    body: JSON.stringify(postData)
+  }).then(resp => getIssues())
 }
 
 function showResults(json) {
@@ -46,11 +60,23 @@ function showResults(json) {
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+
+  fetch(`${baseApi}repos/${repo}/forks`, {
+    method: 'post',
+    headers: {
+      'Authorization': `token ${getToken()}`
+    }
+  }).then(resp => {
+    let repo = new Repo(resp);
+    showForkedRepo(repo);
+  })
 }
 
+function showForkedRepo(repo) {
+  $('#results').append(repo.template())
+}
+
+
 function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
   return ''
 }
